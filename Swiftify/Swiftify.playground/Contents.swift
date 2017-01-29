74f67c6b9e88107cc84470f054537336976088dc
John Doe <xzzz9097@gmail.com>
2017-01-29 23:20:32 +0100

    Start playing with Alamofire and HTTP requests

--- Swiftify/Swiftify.playground/Contents.swift
+++ Swiftify/Swiftify.playground/Contents.swift
@@ -2,5 +2,26 @@
 
 import Cocoa
 import Alamofire
+import PlaygroundSupport
 
-var str = "Hello, playground"
+// Enable infinite execution
+// So that we can get asynchronously the response
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+struct Response {
+    var request:  URLRequest?
+    var response: HTTPURLResponse?
+    var error:    Error?
+}
+
+var aResponse = Response(request: nil, response: nil, error: nil)
+
+Alamofire.request("https://httpbin.org/get").responseJSON { response in    
+    aResponse = Response(request: response.request,
+                         response: response.response,
+                         error: response.error)
+    
+    if let json = response.result.value {
+        print("JSON: \(json)")
+    }
+}
